3a75ad09eecd588005525c48cf1c31dacb5e9770
Administrator <admin@162.243.140.145>
2015-04-17 19:02:05 +0100

    unit tests

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Javascripting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Test t = new Test();
+            Assert.AreEqual(true, t.TestMethod());
+        }
+    }
+}
